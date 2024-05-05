ed4bff6eb9102d46c5172e1a740eee6139bde69f
Benjamin Arntzen <zorlin@gmail.com>
2024-05-05 13:24:19 +0100

    Add DNS creation script

---
+++ gravity.beryju-tools/create-dns.sh
@@ -0,0 +1,15 @@
+for i in {0..63} ; do \
+curl -X 'POST' \
+  "http://10.1.1.151:8008/api/v1/dns/zones/records?zone=riff.cc.&hostname=opal-fragment-$i&uid=0" \
+  -H 'accept: */*' \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer APIKEY' \
+  -d '{
+  "data": "10.2.1.'$i'",
+  "mxPreference": 0,
+  "srvPort": 0,
+  "srvPriority": 0,
+  "srvWeight": 0,
+  "type": "A"
+}'
+; done

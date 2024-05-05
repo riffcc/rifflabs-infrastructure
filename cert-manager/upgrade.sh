75a91ad4cb62474e4c2c542b26aab0c497842106
Benjamin Arntzen <zorlin@gmail.com>
2024-05-05 13:19:18 +0100

    Add cert-manager

---
+++ cert-manager/upgrade.sh
@@ -0,0 +1,6 @@
+helm upgrade --install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.14.3 \
+  --set installCRDs=true -f values.yaml
